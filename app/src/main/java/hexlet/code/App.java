572607765702e6d5bf420f711169c62f105e6572
Maxim Grigorev <zaruv14@yandex.ru>
2022-11-21 22:40:24 +0300

    add some logic for menu's app.

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,41 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
+        Scanner sc = new Scanner(System.in);
+        boolean isWrongAnswer;
+        String greet = "Welcome to the Brain Games!";
 
-        Cli.welcomeGamer();
+        do {
+            isWrongAnswer = false;
+            System.out.println("Please enter the game number and press Enter.");
+            System.out.println("1 - Greet");
+            System.out.println("2 - Even");
+            System.out.println("0 - Exit");
+            System.out.println("3 - Calc");
+            switch (sc.nextInt()) {
+                case 1:
+                    System.out.println(greet);
+                    Cli.welcomeGamer();
+
+                case 2:
+                    System.out.println(greet);
+                    Cli.welcomeGamer();
+                    Games.even();
+
+                case 3:
+                    System.out.println(greet);
+                    Cli.welcomeGamer();;
+                    Games.calculate();
+                case 0:
+                    break;
+
+                default:
+                    System.out.println("choose from 0 to 3");
+                    isWrongAnswer = true;
+            }
+        } while (isWrongAnswer);
     }
 }

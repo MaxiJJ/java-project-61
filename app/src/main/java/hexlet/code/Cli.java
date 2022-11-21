93f8d146e582af30b4ae48557299429634d2510f
Maxim Grigorev <zaruv14@yandex.ru>
2022-11-21 22:42:17 +0300

    add variable name

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,10 +3,17 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
+
+    static String name;
+
     public static void welcomeGamer() {
         System.out.print("May I have your name? ");
         Scanner scanner = new Scanner(System.in);
-        String name = scanner.nextLine();
+        name = scanner.nextLine();
         System.out.println("Hello, " + name + "!");
+    }
+
+    public static String getName() {
+        return name;
     }
 }

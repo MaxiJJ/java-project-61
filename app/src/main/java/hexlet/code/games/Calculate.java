fda2998bfb594bc49c0ef5f4f61ec31b5166009c
Maxim Grigorev <zaruv14@yandex.ru>
2022-12-21 17:39:51 +0300

    refactoring code for checkstyle

--- app/src/main/java/hexlet/code/games/Calculate.java
+++ app/src/main/java/hexlet/code/games/Calculate.java
@@ -1,11 +1,8 @@
 package hexlet.code.games;
 
-import hexlet.code.Cli;
 import hexlet.code.Engine;
-
 import java.util.Random;
 import java.util.Scanner;
-import java.util.stream.IntStream;
 
 public class Calculate {
 
